597c5199a5e64043c743a8f06bc97a17574a290d
Jorge Mendez Ortega <jorge.mendez.ortega@gmail.com>
2020-03-30 13:56:12 -0600

    ➕ADD: Implementando workbox

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-// Validando que exista el serviceWorker
+function log(msg) {
+    const STYLES = "font-size:12px; color:white;background-color:#78c850; padding:3px; border-radius:5px;";
+    console.log("%cregister", STYLES, msg);
+}
+
+log("Validando el uso de navigator.serviceWorker");
 if ("serviceWorker" in navigator) {
-    console.log("Se inicia el registro de poke-worker");
-    navigator
-        .serviceWorker
-            .register("./poke-worker.js")
-                .then(register => console.log("🤟 Se registro exitosamente poke-worker => ", register))
-                .catch(error => console.error("👎 Error registrando poke-worker => ", error));
+    window
+        .addEventListener("load", () => {
+            log("cargando poke-worker");
+            navigator
+                .serviceWorker
+                .register("./poke-worker.js")
+                .then(registration => log(`Service Worker registrado! Scope: ${registration.scope}`))
+                .catch(error => log(`Service Worker registro fallidp!: ${error}`));
+        });
 }
