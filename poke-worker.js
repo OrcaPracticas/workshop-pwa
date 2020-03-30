1752bdc808d8a4ace86292f1d24cdf0b42d2f4fd
Jorge Mendez Ortega <jorge.mendez.ortega@gmail.com>
2020-03-30 14:10:49 -0600

    ADD:  cargando precache

--- poke-worker.js
+++ poke-worker.js
@@ -7,6 +7,78 @@ function log(msg) {
 
 if (workbox) {
     log("WorkBox esta listo 🎉");
+    const revision = "pokemon_001";
+
+    workbox.precaching.precacheAndRoute([
+        {
+            url: "/js/lib/underscore-min.js",
+            revision,
+        },
+        {
+            url: "/js/lib/angular.min.js",
+            revision,
+        },
+        {
+            url: "/js/lib/angular-route.min.js",
+            revision,
+        },
+        {
+            url: "/js/app.js",
+            revision,
+        },
+        {
+            url: "/js/controllers.js",
+            revision,
+        },
+        {
+            url: "/js/directives.js",
+            revision,
+        },
+        {
+            url: "/js/filters.js",
+            revision,
+        },
+        {
+            url: "/js/services.js",
+            revision,
+        },
+        {
+            url: "/script.js",
+            revision,
+        },
+        {
+            url: "/css/bootstrap.min.css",
+            revision,
+        },
+        {
+            url: "/css/bootstrap-theme.min.css",
+            revision,
+        },
+        {
+            url: "/css/main.css",
+            revision,
+        },
+        {
+            url: "/",
+            revision,
+        },
+        {
+            url: "/partials/pokemon-image.html",
+            revision,
+        },
+        {
+            url: "/partials/pokemon-name.html",
+            revision,
+        },
+        {
+            url: "/partials/pokemon-type.html",
+            revision,
+        },
+        {
+            url: "/views/pokedex.html",
+            revision,
+        },
+    ]);
 } else {
     log("WorkBox no esta disponible 🤬");
 }
